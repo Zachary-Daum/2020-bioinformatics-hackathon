34dfefe320f849ed5938881fab848349da9a2908
Zachary.D <zadaum10@gmail.com>
2020-04-24 18:34:54 -0500

    Update authors.py

--- src/authors.py
+++ src/authors.py
@@ -1,3 +1,22 @@
-#get author names and affiliations from /data into list
+import re
+#create file names to draw data from
+with open('data/fake_pmids.txt', 'r') as fakeid:
+    fakeid = fakeid.readlines()
+    fakeid = ''.join(fakeid).replace("PMID:","")
+    idlist = fakeid.splitlines() #by here each PMID is its own item in the list
 
-#search Entrez for # of citations (wont list any more than 20
+    n = 0
+    while ( n <= 5 ):
+        workingid = idlist[n]
+        workingfile = "data/{}.txt"
+        currentfile = workingfile.format(workingid)
+        with open (currentfile, 'rt') as file:
+            workingfile = file.read()
+        #get author names
+            
+        #check for affiliations
+            
+        #search Entrez for # of citations (wont list any more than 20
+            
+        n = n+1
+        
