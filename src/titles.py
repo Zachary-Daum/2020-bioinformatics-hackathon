2f99a7a2f6516691739c434e30e2a35316bdbf49
Zachary.D <zadaum10@gmail.com>
2020-04-25 08:59:50 -0500

    Working Title Retrieval

--- src/titles.py
+++ src/titles.py
@@ -1,3 +1,14 @@
+def between(value, a, b):
+    # Find and validate before-part.
+    pos_a = value.find(a)
+    if pos_a == -1: return ""
+    # Find and validate after part.
+    pos_b = value.rfind(b)
+    if pos_b == -1: return ""
+    # Return middle part.
+    adjusted_pos_a = pos_a + len(a)
+    if adjusted_pos_a >= pos_b: return ""
+    return value[adjusted_pos_a:pos_b]
 #create file names to draw data from
 with open('data/fake_pmids.txt', 'r') as fakeid:
     fakeid = fakeid.readlines()
@@ -5,18 +16,16 @@ with open('data/fake_pmids.txt', 'r') as fakeid:
     idlist = fakeid.splitlines() #by here each PMID is its own item in the list
 
     n = 0
-    while ( n <= 0 ):
+    while ( n <= 20 ):
         workingid = idlist[n]
         workingfile = "data/{}.txt"
         currentfile = workingfile.format(workingid)
         with open (currentfile, 'rt') as file:
-            for line in file:
-                print(line[31])
-                
-            
-        #get title names
-        
-   
-            
+            data = file.read().splitlines()
+            temp = ''.join(data)
+            title = between(temp,'title {        name "','"      },      authors')
+            titlelist = title.splitlines()
+            print(titlelist)
+                   
         n = n+1
         
