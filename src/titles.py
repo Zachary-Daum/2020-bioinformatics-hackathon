4f4a922f650cd569c4ea656ba164631afa1a5cdb
nopethisisloren <64239366+nopethisisloren@users.noreply.github.com>
2020-04-24 22:19:21 -0500

    Create titles.py

---
+++ src/titles.py
@@ -0,0 +1,23 @@
+#create file names to draw data from
+with open('data/fake_pmids.txt', 'r') as fakeid:
+    fakeid = fakeid.readlines()
+    fakeid = ''.join(fakeid).replace("PMID:","")
+    idlist = fakeid.splitlines() #by here each PMID is its own item in the list
+
+    n = 0
+    while ( n <= 5 ):
+        workingid = idlist[n]
+        workingfile = "data/{}.txt"
+        currentfile = workingfile.format(workingid)
+        with open (currentfile, 'rt') as file:
+            workingfile = file.read()
+        #get title names
+
+        slice("Pubmed-entry", "title")
+              
+        slice("authors", "medline")
+              
+        
+            
+        n = n+1
+        
