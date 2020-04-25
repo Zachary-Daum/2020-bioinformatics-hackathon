267bc2a0bdbcab5f3e1e8c120cb44ad3f4a6f53e
Zachary.D <zadaum10@gmail.com>
2020-04-25 09:50:37 -0500

    Working Title PoS Processing

--- src/titles.py
+++ src/titles.py
@@ -1,3 +1,12 @@
+import nltk
+from nltk import pos_tag
+from nltk import RegexpParser
+from nltk.corpus import state_union
+from nltk.tokenize import PunktSentenceTokenizer
+
+train_text = state_union.raw("2005-GWBush.txt")
+custom_sent_tokenizer = PunktSentenceTokenizer(train_text)
+
 def between(value, a, b):
     # Find and validate before-part.
     pos_a = value.find(a)
@@ -16,6 +25,7 @@ with open('data/fake_pmids.txt', 'r') as fakeid:
     idlist = fakeid.splitlines() #by here each PMID is its own item in the list
 
     n = 0
+    titlelist = []
     while ( n <= 20 ):
         workingid = idlist[n]
         workingfile = "data/{}.txt"
@@ -24,8 +34,30 @@ with open('data/fake_pmids.txt', 'r') as fakeid:
             data = file.read().splitlines()
             temp = ''.join(data)
             title = between(temp,'title {        name "','"      },      authors')
-            titlelist = title.splitlines()
-            print(titlelist)
-                   
+            
+        #build lists into one
+        workinglist = title.splitlines()
+        titlelist = titlelist + workinglist
         n = n+1
+    i = 0
+    while ( i < n ):
+        workingtitle = titlelist[i]
+        print(workingtitle)
+        #analyze PoS of titles
+        tokenized = custom_sent_tokenizer.tokenize(workingtitle)
+        def process_content():
+            try:
+                for k in tokenized[:5]:
+                    words = nltk.word_tokenize(k)
+                    tagged = nltk.pos_tag(words)
+                    print(tagged)
+
+            except Exception as e:
+                print(str(e))
+        process_content()
+        
+        i = i+1
+
+        #find PoS percentages
+        
         
