98df299d0f015bbd8c75bc13c0ca752b7ef9d90b
Zachary.D <zadaum10@gmail.com>
2020-04-24 23:42:38 -0500

    Parsing Testing

--- testing/parsing-test/parsing-test.py
+++ testing/parsing-test/parsing-test.py
@@ -11,23 +11,38 @@ def between(value, a, b):
     if adjusted_pos_a >= pos_b: return ""
     return value[adjusted_pos_a:pos_b]
 
-#get data
-with open('test-data2.txt','r') as file:
-    data = file.read().splitlines()
-    temp = ''.join(data)
-    if "names ml" in temp:
-        #w/out affiliations
-        authors = between(temp, 'names ml {          "', '"        }      },').replace('",          "',"-")
-        #list author's names
-        authorlist = authors.split("-")
-        print(authorlist)
+#working w/ multiple files
+n = 1
+while ( n <= 2 ):
+    workingfile = "test-data{}.txt"
+    workingfile = workingfile.format(n)
+
+    #get data
+    with open(workingfile,'r') as file:
+        data = file.read().splitlines()
+        temp = ''.join(data)
+        if "names ml" in temp:
+            #w/out affiliations
+            authors = between(temp, 'names ml {          "', '"        }      },').replace('",          "',"-")
+            #list author's names
+            authorlist = authors.split("-")
+            print(authorlist)
+
+            #still need to find lookup method for authors
+            n = n+1
         
-    else:
-        #get needed data
-        data = between(temp, "names std {          {",'          }        }      },      from journal').replace('."          },          {            name ml ','-').replace('",            affil str "','-').replace('            name ml "','').replace('"','')
-        datasplit = data.split("-")
-        #find each author
-        AUTH = datasplit[0::2]
-        #find each author's affiliations
-        AFF = datasplit[1::2]
+        else:
+            #get needed data
+            data = between(temp, "names std {          {",'          }        }      },      from journal').replace('."          },          {            name ml ','-').replace('",            affil str "','-').replace('            name ml "','').replace('"','')
+            datasplit = data.split("-")
+            #find each author
+            AUTH = datasplit[0::2]
+            #find each author's affiliations
+            AFF = datasplit[1::2]
+            print(AUTH)
+            print(AFF)
+
+            #lookup authors and affiliations and compile in other doc
+            n = n+1
+
 
