b8266067946274438684b977717394b4c8bf80d9
nopethisisloren <64239366+nopethisisloren@users.noreply.github.com>
2020-04-25 10:07:31 -0500

    Create titles copy.py

---
+++ src/titles copy.py
@@ -0,0 +1,40 @@
+def between(value, a, b):
+    # Find and validate before-part.
+    pos_a = value.find(a)
+    if pos_a == -1: return ""
+    # Find and validate after part.
+    pos_b = value.rfind(b)
+    if pos_b == -1: return ""
+    # Return middle part.
+    adjusted_pos_a = pos_a + len(a)
+    if adjusted_pos_a >= pos_b: return ""
+    return value[adjusted_pos_a:pos_b]
+#create file names to draw data from
+with open('data/fake_pmids.txt', 'r') as fakeid:
+    fakeid = fakeid.readlines()
+    fakeid = ''.join(fakeid).replace("PMID:","")
+    idlist = fakeid.splitlines() #by here each PMID is its own item in the list
+
+    n = 0
+    while ( n <= 20 ):
+        workingid = idlist[n]
+        workingfile = "test-data{}.txt"
+        currentfile = workingfile.format(workingid)
+                
+            
+        #get title names
+        with open(workingfile, 'r') as file:
+            data = file.read().splitlines()
+            temp = ''.join(data)
+            titles = between(temp, 'cit {          title {          name"', '"        }')
+            print(titles)
+   
+            
+        with open (currentfile, 'rt') as file:
+            data = file.read().splitlines()
+            temp = ''.join(data)
+            title = between(temp,'title {        name "','"      },      authors')
+            titlelist = title.splitlines()
+            print(titlelist)
+                   
+        n = n+1
